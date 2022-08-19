fa8875ee01aadefa6d2f8c08a617d82102a264e7
Allochka25 <alkun@mail.ru>
2022-08-19 17:53:08 +0300

    update ДЗ_1.3.py

---
+++ ДЗ_1.3.py
@@ -0,0 +1,10 @@
+# 10.3
+# Дан словарь:
+# days = { 1:'Sun', 2:'Mon', 3:'Tue', 4:'Wed', 5:'Thu', 6:'Fri', 7:’Sat’}
+# Записать его в файл построчно.
+
+days = {1: 'Sun', 2: 'Mon', 3: 'Tue', 4: 'Wed', 5: 'Thu', 6: 'Fri', 7: 'Sat'}
+with open("ДЗ_11.txt", "w") as f:
+    for k, v in days.items():
+        f.write("{}:{}\n".format(k,v))
+print()
